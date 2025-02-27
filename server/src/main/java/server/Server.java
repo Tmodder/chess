b8ebb325ad3761470bf17c3edefa0649e6ba4fe4
Thomas Esplin <tom.esplin@gmail.com>
2025-02-27 10:18:11 -0700

    start the first endpoint(register)

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,5 @@
 package server;
-
+import handler.*;
 import spark.*;
 import com.google.gson.Gson;
 public class Server {
@@ -13,6 +13,8 @@ public class Server {
 
         //This line initializes the server and can be removed once you have a functioning endpoint
        // Spark.init();
+        var userHandler = new UserHandler();
+        Spark.post("/user", userHandler::register);
         Spark.delete("/db", (req, res) -> "{\"name\":\"John\", \"age\":30, \"car\":null}");
         //do the same for post and each method repeating if there is a different path
 
