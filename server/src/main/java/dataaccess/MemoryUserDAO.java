46acc8dd73fd4db77ac2fb13796a14ee5ef6e86e
Thomas Esplin <tom.esplin@gmail.com>
2025-02-28 10:24:34 -0700

    Add implementation of interface UserDAO in MemoryUserDAO

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,35 @@
+package dataaccess;
+
+import model.User;
+
+import java.util.HashSet;
+
+public class MemoryUserDAO implements UserDAO{
+    private HashSet<User> userDatabase = new HashSet<>();
+    @Override
+    public void createUser(String username, String password, String email) {
+        var newUser = new User(username, password, email);
+        userDatabase.add(newUser);
+    }
+
+    @Override
+    public User findUser(String username) {
+        for (User i : userDatabase)
+        {
+            if (i.username().equals(username))
+            {
+                return i;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteUser(String username) throws DataAccessException {
+        User u = findUser(username);
+        if (u != null)
+        {
+            userDatabase.remove(u);
+        }
+    }
+}
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,9 @@
 package dataaccess;
+import model.User;
 
 public interface UserDAO {
+
+    void createUser(String username, String password, String email);
+    User findUser(String username);
+    void deleteUser(String username)  throws DataAccessException;
 }
