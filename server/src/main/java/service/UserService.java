e4376c227b2c5d8f030a4351e11997121114ebab
Thomas Esplin <tom.esplin@gmail.com>
2025-02-28 09:59:30 -0700

    Add basic framework for UserService

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -2,11 +2,41 @@ package service;
 
 import RequestResult.RegisterRequest;
 import RequestResult.RegisterResult;
+import model.User;
+import model.Authtoken;
+import java.util.UUID;
+
 
 public class UserService {
     public static RegisterResult registerService(RegisterRequest req)
     {
-        // run data stuff
+        // getUser()
+        // if null
+        // createUser()
+        // createAuth()
+        // return new RegisterResult()
         return null;
     }
+
+    public static User getUser(String username)
+    {
+
+        return null;
+    }
+
+    public static User createUser()
+    {
+        return null;
+    }
+
+    public static Authtoken createAuth()
+    {
+        generateToken();
+        return null;
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
 }
