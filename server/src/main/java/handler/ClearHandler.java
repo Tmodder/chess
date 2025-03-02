db9fd09788e4c3cac49d1c73f58a424cf55d2eac
Thomas Esplin <tom.esplin@gmail.com>
2025-03-02 15:56:57 -0700

    Start UserServiceTests and rename clear in ClearService to runClear

--- server/src/main/java/handler/ClearHandler.java
+++ server/src/main/java/handler/ClearHandler.java
@@ -7,7 +7,7 @@ import spark.Response;
 public class ClearHandler extends Handler{
     public Object clear(Request request, Response response)
     {
-        ClearService.clear();
+        ClearService.runClear();
         return "null";
     }
 }
