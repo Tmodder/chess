42da64bb05070634dbda4c8df27b790700347469
Thomas Esplin <tom.esplin@gmail.com>
2025-03-21 13:20:27 -0600

    Add ServerFacade and ClientCommunicator

---
+++ client/src/main/java/ui/ServerFacade.java
@@ -0,0 +1,41 @@
+package ui;
+
+public class ServerFacade
+{
+    void login()
+    {
+        //makeRequest()
+    }
+
+    void register()
+    {
+
+    }
+
+    void logout()
+    {
+
+    }
+
+    void createGame()
+    {
+
+    }
+
+    void listGames()
+    {
+
+    }
+
+
+    void playGame()
+    {
+
+    }
+
+    void observeGame ()
+    {
+
+    }
+
+}
