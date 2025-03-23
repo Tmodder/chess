9580b1dfe42b18f76134335c9fd66a3798c92ae6
Thomas Esplin <tom.esplin@gmail.com>
2025-03-22 18:19:57 -0600

    Add first method bodies for user endpoints

--- client/src/main/java/ServerFacade.java
+++ client/src/main/java/ServerFacade.java
@@ -1,22 +1,30 @@
+import requestandresult.*;
+
 import java.util.ArrayList;
 
 public class ServerFacade
 {
     private final ArrayList<Integer> gameIdList = new ArrayList<>();
+    private final  ClientCommunicator communicator = new ClientCommunicator("http/localhost:8080");
+    private String authToken;
     void login(String username, String password)
     {
-        //create request object and pass it into the make request
-        //makeRequest("POST",)
+        var res = communicator.makeRequest("POST","/session", new LoginRequest(username,password), LoginResult.class);
+        authToken = res.authToken();
+
     }
 
-    void register()
+    void register(String username, String password, String email)
     {
-
+        var res = communicator.makeRequest("POST","/user",new RegisterRequest(username, password, email), RegisterResult.class);
+        authToken = res.authToken();
     }
 
     void logout()
     {
-
+        assert authToken != null;
+        var req = communicator.makeRequest("POST", "/session", new LogoutRequest(authToken),null);
+        authToken = null;
     }
 
     void createGame()
