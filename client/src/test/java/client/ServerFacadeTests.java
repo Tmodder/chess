eaa56835de2a3fc1e5e56c4e2b2dc6818afdbb4e
Thomas Esplin <tom.esplin@gmail.com>
2025-04-26 11:18:34 -0600

    Add client jar

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -25,7 +25,7 @@ public class ServerFacadeTests {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
-        facade = new ServerFacade(port,null);
+        facade = new ServerFacade("localhost:" + port,null);
     }
 
     @AfterAll
